0fe2a145d025639357be6eec5fc857eec8a4f856
pyogeswarsai <pyogeswarsai1249@gmail.com>
2025-09-19 15:49:52 +0530

    changes in pages with components

--- src/pages/HiringSection.jsx
+++ src/pages/HiringSection.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Hero_hire from '../Components/Hero_hire'
-import Wedo from '../Components/Wedo'
-import Jobcart from '../Components/Jobcart'
-import CareerPrep from '../Components/CareerPrep'
-import Testimonials from '../Components/Testimonials'
-import Getintoch from '../Components/Getintoch';
-import JobApplication from "./JobApplication"
+import Hero_hire from '../components/Hero_hire'
+import Wedo from '../components/Wedo'
+import Jobcart from '../components/Jobcart'
+import CareerPrep from '../components/CareerPrep'
+import Testimonials from '../components/Testimonials'
+import Getintoch from '../components/Getintoch';
 import HomeNavbar from '../components/HomeNavbar';
 import Homefooter from '../components/Homefooter';
 const HiringSection = () => {
